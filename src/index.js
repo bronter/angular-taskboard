0f75fcd36f59c85c03b0334939958a8d4fa50d8b
Gabriel Charleboix <gcharleboix@gmail.com>
2016-12-15 03:27:38 -0800

    home -> board

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ngRoute from "angular-route";
 import 'font-awesome-sass-loader';
 import "../styles/global.scss";
 
-import HomeController from "./controllers/home.js";
+import BoardController from "./controllers/board.js";
 import ColumnsController from "./controllers/columns.js";
 import ColumnController from "./controllers/column.js";
 import TasksController from "./controllers/tasks.js";
 import TaskController from "./controllers/task.js";
 
-import Home from "./templates/home.html";
+import Board from "./templates/board.html";
 import Columns from "./templates/columns.html";
 import Column from "./templates/column.html";
 import Tasks from "./templates/tasks.html";
@@ -19,10 +19,10 @@ const testModule = angular.module('taskboard', ["ngRoute"]);
 testModule.config(($routeProvider) => {
   $routeProvider
     .when("/", {
-      template: Home,
+      template: Board,
     });
 });
-testModule.controller('home', HomeController);
+testModule.controller('board', BoardController);
 testModule.controller('taskColumn', ColumnsController);
 testModule.controller('tasks', TasksController);
 testModule.controller('task', TaskController);
